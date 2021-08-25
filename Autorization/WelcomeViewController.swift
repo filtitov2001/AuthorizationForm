3b266dfc089fc7dde26a14521d50d743f6d03cc0
Felix Titov <titow.feliks@yandex.ru>
2021-08-26 00:24:20 +0300

    added gradient

--- Autorization/WelcomeViewController.swift
+++ Autorization/WelcomeViewController.swift
@@ -7,16 +7,28 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeMessageLabel: UILabel!
     
-    
     var welcomeMessage: String!
+    @IBOutlet var elementsStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeMessageLabel.text = welcomeMessage
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.blue.cgColor
+        ]
+        view.layer.addSublayer(gradientLayer)
+        
+        view.bringSubviewToFront(elementsStackView)
+        
     }
     
     
